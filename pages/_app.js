4e46dcba1f836f3f704a18a929c66a5c769f88a1
Всеволод <kreepmeister@gmail.com>
2023-07-11 22:46:44 +0300

    👤 Add social media meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -8,10 +8,21 @@ export default function App({ Component, pageProps }) {
             <Head>
                 <title>vsecoder</title>
                 <link rel="shortcut icon" href="/favicon.ico" />
+
                 <meta
                     name="viewport"
                     content="minimum-scale=1, initial-scale=1, width=device-width"
                 />
+                <meta property="twitter:image" content="/image.png" />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:title" content="vsecoder" />
+                <meta property="twitter:description" content="@vsecoder website" />
+                <meta property="author" content="vsecoder" />
+                <meta property="description" content="@vsecoder website" />
+                <meta property="og:image" content="/image.png" />
+                <meta property="og:title" content="vsecoder" />
+                <meta property="og:description" content="@vsecoder website" />
+                <meta property="og:url" content="https://vsecoder.me" />
             </Head>
 
             <Analytics />
