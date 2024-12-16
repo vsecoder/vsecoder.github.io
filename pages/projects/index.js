801b6aac5af72d2bda2e86ba7dca735181a7ef13
Всеволод О. <kreepmeister@gmail.com>
2024-12-16 13:13:26 +0300

    🗑 Remove SSR (not work on vercel)

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,45 +1,33 @@
 import styles from '../index.module.css';
 import { Canvas3d } from '@/components/3d';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
-export async function getServerSideProps() {
-  try {
-    const response = await fetch('http://localhost:3000/api/projects');
-    const data = await response.json();
 
-    return {
-      props: {
-        projects: data.projects || [],
-      },
-    };
-  } catch (error) {
-    return {
-      props: {
-        projects: 'Error loading markdown.',
-      },
-    };
-  }
-}
+export default function App() {
+  const [projects, setProjects] = useState(['Loading...']);
 
-export default function App({ projects }) {
+  useEffect(() => {
+    fetch('/api/projects')
+      .then((response) => response.json())
+      .then((data) => {
+        setProjects(data.projects);
+      })
+      .catch((error) => {
+        setProjects('Error loading markdown.');
+      });
+  }, []);
+
   return (
     <>
       <div className={styles.container}>
         <Link href="/">&lt; Back</Link>
         <h2>Projects:</h2>
         <ul>
-          {Array.isArray(projects) ? (
-            projects.map((element, i) => {
-              element = element.replace('.md', '');
-              return (
-                <li key={i}>
-                  <Link href={`/projects/${element}`}>{element}</Link>
-                </li>
-              );
-            })
-          ) : (
-            <li>{projects}</li>
-          )}
+          {projects.map((element, i) => {
+            element = element.replace('.md', '');
+            return (<li key={i}><Link href={`/projects/${element}`}>{element}</Link></li>)
+          })}
         </ul>
         <p>Later more.</p>
       </div>
